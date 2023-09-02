bae84b6d6abcb3bddb4f7716c8f668d88791d6d9
Anupam Yadav <104265474+anupamyada@users.noreply.github.com>
2023-09-02 15:36:38 +0530

    Add files via upload

---
+++ ASSIGNMENT/DATA_STRUCTURE/ASSIGNMENT_DS01.py
@@ -0,0 +1,21 @@
+# 1> Write a program to sum all the element in a list
+l = [1,2,3,4,5,6,7,8,9,10]
+sum = sum(l)
+print("SUM" , sum)
+
+# 2> write a program to multiply all the items in a list with 3
+#and print it.
+
+l=[1,2,3,4,5]
+l1=[]
+for i in l:
+    x=i*2
+    l1.append(x)
+print("Multiply:",l1)
+
+
+# 3> write a python program to get the largest number from a list.
+l = [1,2,3,4,5]
+x=max(l)
+print("Max Value:",x)
+
