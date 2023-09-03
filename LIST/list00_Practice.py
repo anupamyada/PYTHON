a6d8fef6159a9dee83aad65a277548e34166b86c
Anupam Yadav <104265474+anupamyada@users.noreply.github.com>
2023-09-03 18:17:17 +0530

    Add files via upload

---
+++ LIST/list00_Practice.py
@@ -0,0 +1,17 @@
+##l1 = ["apple","banana","grapes"]
+##l1[1]="guava"
+##print(l1)
+l2=[1,2,3,4,5,6]
+##l2[1:3]= 7,8,9,10,10,10
+print(l2)
+##l2[1:3]=2,3 # getting error while assigning only one value
+
+##print(l2)
+##print(l2.insert("false"))
+print(len(l2))
+l3 = ["apple", "banana","cherry"]
+l3.insert(2,"watermelon")
+print(l3)
+l4 = ["apple","banana","cherry"]
+l4[1:3] = ["watermelon"]
+print(l4)
---
+++ LIST/lists_tuple.py
@@ -0,0 +1,42 @@
+'''# class definition
+class Student:
+   def _init_(self, fname, lname, age, section):
+       self.firstname = fname
+       self.lastname = lname
+       self.age = age
+       self.section = section
+# creating a new object
+stu1 = Student("Sara", "Ansh", 22, "A2") '''
+
+
+'''# class definition
+class Student:
+    def _init_(self,fname,lname,age,section):
+        self.firstname = fname
+        self.lastname = lname
+        self.age = age
+        self.section = section
+        print("self.firstname",)
+
+# creating a new object
+_init_
+stu1 = Student("Sara", "Ansh", 22, "A2")'''
+
+'''x=5
+y=1+x**2
+print(y)
+y=y-6
+print(y)
+
+z=y+3
+print(z)
+
+q=1.6
+r=round(q)
+print(r)'''
+
+'''a=input()
+print(a)'''
+name = input("enter your name: ")
+print(name)
+
